d921e7faf3b2701420e519678cd9a87a135d2a87
Allen Nguyen <anhn321@gmail.com>
2023-08-04 01:46:45 -0600

    Finish home page with all content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,76 @@
+import img from "../assets/home.jpg";
+import { BsInstagram } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { BsDiscord } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
+
 function Home() {
+  const iconSize = 30;
+
+  const contactLinks = [
+    {
+      name: "Instagram",
+      href: "https://instagram.com/css.ucalgary/",
+      icon: <BsInstagram size={iconSize} />,
+    },
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/uofccss",
+      icon: <BsFacebook size={iconSize} />,
+    },
+    {
+      name: "Discord",
+      href: "https://discord.gg/n3amXwbAaW",
+      icon: <BsDiscord size={iconSize} />,
+    },
+    {
+      name: "Email",
+      href: "mailto:css.uofcalgary@gmail.com",
+      icon: <FiMail size={iconSize} />,
+    },
+  ];
+
+  const contactIcons = contactLinks.map((contact) => (
+    <a
+      key={contact.name}
+      href={contact.href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {contact.icon}
+    </a>
+  ));
+
   return (
-    <>
-      <h1>Chinese Students&apos; Society</h1>
-      <p>Dedicated to promoting Chinese culture since 1968</p>
-      <button>JOIN US</button>
-    </>
+    <div className="flex flex-auto justify-center items-center gap-10 border-gray-500 border-2 mt-4 m-12 p-8 rounded-lg font-agrandir">
+      <div className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-6xl font-semibold font-agrandir">
+          Chinese Students&apos; Society
+        </h1>
+
+        <p className="text-xl">
+          Dedicated to promoting Chinese culture since 1968
+        </p>
+
+        <a
+          href="https://forms.gle/TEfPJJGGk8kFCDmC8"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-black px-12 py-2 text-white font-semibold rounded-lg my-4"
+        >
+          JOIN US
+        </a>
+
+        <svg height="10" width="60%" className="m-4">
+          <line x1="100%" stroke="black" strokeWidth="3" />
+        </svg>
+
+        <p className="font-semibold">FOLLOW US ON SOCIAL MEDIA</p>
+
+        <div className="flex gap-8">{contactIcons}</div>
+      </div>
+      <img src={img} alt="" className="h-96 rounded-lg" />
+    </div>
   );
 }
 
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,17 +1,18 @@
 import { Outlet, Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Layout() {
   return (
     <>
-      <nav className="flex justify-center mt-4">
-        <ul className="flex gap-8 font-semibold">
-          <Link to="/">Home</Link>
-          <Link to="/about-us">About Us</Link>
-          <Link to="/events">Events</Link>
-          <Link to="/get-involved">Get Involved</Link>
-          <Link to="/resources">Resources</Link>
-          <Link to="/contact-us">Contact Us</Link>
-        </ul>
+      <nav className="flex justify-evenly items-center pt-8 gap-8 font-bold text-lg font-agrandir">
+        <Link to="/">
+          <img src={logo} alt="CSS logo" className="h-16" />
+        </Link>
+        <Link to="/about-us">ABOUT US</Link>
+        <Link to="/events">EVENTS</Link>
+        <Link to="/get-involved">GET INVOLVED</Link>
+        <Link to="/resources">RESOURCES</Link>
+        <Link to="/contact-us">CONTACT US</Link>
       </nav>
       <Outlet />
     </>
