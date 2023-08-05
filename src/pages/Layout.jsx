2de506459e0f3d50df7eeae6983145c45e97552a
Allen Nguyen <anhn321@gmail.com>
2023-08-05 01:06:38 -0600

    Make navbar responsive

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,18 +1,55 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 export default function Layout() {
+  const links = [
+    {
+      id: 1,
+      content: "ABOUT US",
+      to: "/about-us",
+    },
+    {
+      id: 2,
+      content: "EVENTS",
+      to: "/events",
+    },
+    {
+      id: 3,
+      content: "GET INVOLVED",
+      to: "/get-involved",
+    },
+    {
+      id: 4,
+      content: "RESOURCES",
+      to: "/resources",
+    },
+    {
+      id: 5,
+      content: "CONTACT US",
+      to: "/contact-us",
+    },
+  ];
+
   return (
     <>
-      <nav className="flex justify-evenly items-center pt-8 gap-8 font-bold text-lg font-agrandir">
+      <nav className="flex justify-evenly items-center pt-8 gap-8 font-bold text-lg font-agrandir ">
         <Link to="/">
           <img src={logo} alt="CSS logo" className="h-16" />
         </Link>
-        <Link to="/about-us">ABOUT US</Link>
-        <Link to="/events">EVENTS</Link>
-        <Link to="/get-involved">GET INVOLVED</Link>
-        <Link to="/resources">RESOURCES</Link>
-        <Link to="/contact-us">CONTACT US</Link>
+        {links.map((link) => (
+          <NavLink
+            to={link.to}
+            key={link.id}
+            className="hover:bg-gray-400 rounded-lg px-8 py-4"
+            style={({ isActive }) => {
+              return {
+                backgroundColor: isActive ? "red" : "",
+              };
+            }}
+          >
+            {link.content}
+          </NavLink>
+        ))}
       </nav>
       <Outlet />
     </>
