ffe01cffa264f563598c338eb7ae1812e0491657
Allen Nguyen <anhn321@gmail.com>
2023-08-21 02:04:23 -0600

    Style contact page for mobile

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,12 +3,12 @@ import logo from "../assets/logo.png";
 
 function ContactUs() {
   return (
-    <section className="flex flex-col flex-auto m-8 justify-between">
-      <h1 className="text-7xl font-bold mb-8">
+    <section className="flex flex-col flex-auto m-4 lg:m-8 justify-between">
+      <h1 className="text-4xl lg:text-7xl font-bold mb-8">
         <span className="text-red">Contact</span> Us
       </h1>
-      <div className="flex justify-between items-center">
-        <div className="flex flex-col gap-6 text-xl">
+      <div className="flex flex-col-reverse lg:flex-row justify-between items-center">
+        <div className="flex flex-col gap-4 lg:gap-6 text-base lg:text-xl">
           <p className="font-bold">LOCATION</p>
           <p>University of Calgary, 2500 University Dr NW, Calgary</p>
           <p className="font-bold">EMAIL</p>
@@ -16,7 +16,11 @@ function ContactUs() {
           <p className="font-bold">SOCIAL</p>
           <ContactIcons />
         </div>
-        <img src={logo} alt="CSS logo" className="h-64" />
+        <img
+          src={logo}
+          alt="CSS logo"
+          className="hidden lg:inline-block h-64"
+        />
       </div>
     </section>
   );
