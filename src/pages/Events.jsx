b47c55936277add5cafbce9ff125a780f9ba6c92
Allen Nguyen <anhn321@gmail.com>
2023-10-10 20:17:41 -0700

    Add fall icebreaker 2023 to events diary

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -10,6 +10,9 @@ import LNYPhotoContest2022 from "../assets/images/events-diary/lny-photo-contest
 import MeetAndGreet2022 from "../assets/images/events-diary/meet-and-greet-2022.jpg";
 import MeetAndGreet2021 from "../assets/images/events-diary/meet-and-greet-2021.jpg";
 import Diversitea from "../assets/images/events-diary/diversitea.jpg";
+import FallIcebreaker2023_1 from "../assets/images/events-diary/fall-icebreaker-2023(1).jpg";
+import FallIcebreaker2023_2 from "../assets/images/events-diary/fall-icebreaker-2023(2).jpg";
+import FallIcebreaker2023_3 from "../assets/images/events-diary/fall-icebreaker-2023(3).jpg";
 
 import { Card, CardBody, Image, Stack, Text } from "@chakra-ui/react";
 import PropTypes from "prop-types";
@@ -18,27 +21,27 @@ import SignUpButton from "../components/SignUpButton";
 const events = [
   {
     id: 0,
-    name: "End of Year Party",
-    date: "April 6, 2023",
-    img: YearEndParty2023,
+    name: "Fall Welcome Event",
+    date: "October 22, 2021",
+    img: MeetAndGreet2021,
   },
   {
     id: 1,
-    name: "Diversitea",
-    date: "January 20, 2023",
-    img: Diversitea,
+    name: "New Years Photo Contest x CGSA",
+    date: "February 5, 2022",
+    img: LNYPhotoContest2022,
   },
   {
     id: 2,
-    name: "Lunar New Year CSS x CLCC",
-    date: "January 27, 2023",
-    img: LNY2023,
+    name: "Lunar New Year",
+    date: "February 6, 2022",
+    img: LNY2022,
   },
   {
     id: 3,
-    name: "Octopus Friday Games",
-    date: "November 25, 2022",
-    img: OctopusFridayGames,
+    name: "Year End Party",
+    date: "April 1, 2022",
+    img: YearEndParty2022,
   },
   {
     id: 4,
@@ -48,27 +51,33 @@ const events = [
   },
   {
     id: 5,
-    name: "Year End Party",
-    date: "April 1, 2022",
-    img: YearEndParty2022,
+    name: "Octopus Friday Games",
+    date: "November 25, 2022",
+    img: OctopusFridayGames,
   },
   {
     id: 6,
-    name: "Lunar New Year",
-    date: "February 6, 2022",
-    img: LNY2022,
+    name: "Lunar New Year CSS x CLCC",
+    date: "January 27, 2023",
+    img: LNY2023,
   },
   {
     id: 7,
-    name: "New Years Photo Contest x CGSA",
-    date: "February 5, 2022",
-    img: LNYPhotoContest2022,
+    name: "Diversitea",
+    date: "January 20, 2023",
+    img: Diversitea,
   },
   {
     id: 8,
-    name: "Fall Welcome Event",
-    date: "October 22, 2021",
-    img: MeetAndGreet2021,
+    name: "End of Year Party",
+    date: "April 6, 2023",
+    img: YearEndParty2023,
+  },
+  {
+    id: 9,
+    name: "Fall Icebreaker",
+    date: "October 3, 2023",
+    img: FallIcebreaker2023_3,
   },
 ];
 
@@ -120,7 +129,7 @@ export default function Events() {
           <span className="text-red">Events</span> Diary
         </h1>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-center justify-items-center">
-          {events.map((e) => (
+          {events.toReversed().map((e) => (
             <DiaryEvent key={e.id} event={e} />
           ))}
         </div>
