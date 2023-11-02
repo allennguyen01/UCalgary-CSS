d576dfe150ff74d9404a2415237b10375f565871
Allen Nguyen <anhn321@gmail.com>
2023-11-02 00:00:39 -0700

    Organize events diary with tabs by school year

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -16,69 +16,102 @@ import FallIcebreaker2023_1 from "../assets/images/events-diary/fall-icebreaker-
 import FallIcebreaker2023_2 from "../assets/images/events-diary/fall-icebreaker-2023(2).jpg";
 import FallIcebreaker2023_3 from "../assets/images/events-diary/fall-icebreaker-2023(3).jpg";
 
-import { Card, CardBody, Image, Stack, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  extendTheme,
+} from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import SignUpButton from "../components/SignUpButton";
+
+const breakpoints = {
+  base: "0px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
+
+extendTheme({ breakpoints });
 
 const events = [
   {
     id: 0,
     name: "Fall Welcome Event",
     date: "October 22, 2021",
+    schoolYear: 2021,
     img: MeetAndGreet2021,
   },
   {
     id: 1,
     name: "New Years Photo Contest x CGSA",
     date: "February 5, 2022",
+    schoolYear: 2021,
     img: LNYPhotoContest2022,
   },
   {
     id: 2,
     name: "Lunar New Year",
     date: "February 6, 2022",
+    schoolYear: 2021,
     img: LNY2022,
   },
   {
     id: 3,
     name: "Year End Party",
     date: "April 1, 2022",
+    schoolYear: 2021,
     img: YearEndParty2022,
   },
   {
     id: 4,
     name: "Meet & Greet",
     date: "October 14, 2022",
+    schoolYear: 2022,
     img: MeetAndGreet2022,
   },
   {
     id: 5,
     name: "Octopus Friday Games",
     date: "November 25, 2022",
+    schoolYear: 2022,
     img: OctopusFridayGames,
   },
   {
     id: 6,
     name: "Lunar New Year CSS x CLCC",
     date: "January 27, 2023",
+    schoolYear: 2022,
     img: LNY2023,
   },
   {
     id: 7,
     name: "Diversitea",
     date: "January 20, 2023",
+    schoolYear: 2022,
     img: Diversitea,
   },
   {
     id: 8,
     name: "End of Year Party",
     date: "April 6, 2023",
+    schoolYear: 2022,
     img: YearEndParty2023,
   },
   {
     id: 9,
     name: "Fall Icebreaker",
     date: "October 3, 2023",
+    schoolYear: 2023,
     img: FallIcebreaker2023_3,
   },
 ];
@@ -117,11 +150,51 @@ export default function Events() {
         <h1 className="text-3xl lg:text-5xl font-bold">
           <span className="text-red">Events</span> Diary
         </h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-center justify-items-center">
-          {events.toReversed().map((e) => (
-            <DiaryEvent key={e.id} event={e} />
-          ))}
-        </div>
+
+        <Tabs
+          isFitted
+          variant="soft-rounded"
+          colorScheme="red"
+          size={{ sm: "sm", lg: "md" }}
+        >
+          <TabList>
+            <Tab>2023/2024</Tab>
+            <Tab>2022/2023</Tab>
+            <Tab>2021/2022</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-center justify-items-center">
+                {events
+                  .toReversed()
+                  .filter((e) => e.schoolYear == 2023)
+                  .map((e) => (
+                    <DiaryEvent key={e.id} event={e} />
+                  ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-center justify-items-center">
+                {events
+                  .toReversed()
+                  .filter((e) => e.schoolYear == 2022)
+                  .map((e) => (
+                    <DiaryEvent key={e.id} event={e} />
+                  ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-center justify-items-center">
+                {events
+                  .toReversed()
+                  .filter((e) => e.schoolYear == 2021)
+                  .map((e) => (
+                    <DiaryEvent key={e.id} event={e} />
+                  ))}
+              </div>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </section>
     </main>
   );
